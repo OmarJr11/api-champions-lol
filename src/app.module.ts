07d45cfd1895135d5ab4e672563958d9d3f32c20
Omar <omaryoeljr@gmail.com>
2022-11-19 16:03:57 -0400

    :art: develop feat(Develop) Add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+require('dotenv').config();
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@${process.env.DATABASE}?retryWrites=true&w=majority`
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
